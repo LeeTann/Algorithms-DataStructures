fb8eae3d3f20dd23c476695f1839221ad3c22706
ltann <leeroyleeroy18@gmail.com>
2020-04-06 20:44:39 -0700

    group anagrams answer two

--- javascript/group_anagrams.js
+++ javascript/group_anagrams.js
@@ -15,30 +15,48 @@
 // The order of your output does not matter.
 
 // ANSWER # 1 ////
-var groupAnagrams = function(strs) {
-    const hash = {}
+// var groupAnagrams = function(strs) {
+//     const hash = {}
     
-    // loop thru the array
-    for (let str of strs) {
-      console.log(str)
-      let sum = 0
+//     // loop thru the array
+//     for (let str of strs) {
+//       console.log(str)
+//       let sum = 0
 
-      // loop thru each string and sum up the charcode of each char
-      for (let i = 0; i < str.length; i++) {
-        sum += str.charCodeAt(i) ** 4
-      }
-      console.log(sum)
+//       // loop thru each string and sum up the charcode of each char
+//       for (let i = 0; i < str.length; i++) {
+//         sum += str.charCodeAt(i) ** 4
+//       }
+//       console.log(sum)
 
-      // check if the sum not in hash, create an empty array of that hash sum
-      if (!hash[sum]) {
-        hash[sum] = []
-        hash[sum].push(str)
-      } else {
-        hash[sum].push(str)
-      }
+//       // check if the sum not in hash, create an empty array of that hash sum
+//       if (!hash[sum]) {
+//         hash[sum] = []
+//         hash[sum].push(str)
+//       } else {
+//         hash[sum].push(str)
+//       }
+//     }
+//     console.log(Object.values(hash))
+//     return Object.values(hash)
+// };
+
+//// ANSWER # 2 ////
+let groupAnagrams = function(strs) {
+  let hash = {}
+  
+  for (let str of strs) {
+    const sortedStr = str.split('').sort((a, b) => a > b).join('')
+    
+    if (!hash[sortedStr]) {
+      hash[sortedStr] = []
+      hash[sortedStr].push(str)
+    } else {
+      hash[sortedStr].push(str)
     }
-    console.log(Object.values(hash))
-    return Object.values(hash)
-};
+  }
+  console.log(Object.values(hash))
+  return(Object.values(hash))
+}
 
-groupAnagrams(["run","had","lot","kim","fat","net","fin","rca","chi","lei","lox","iva","liz","hug","hot","irk","lap","tan","tux","yuk","hep","map","ran","ell","kit","put","non","aol","add","lad","she","job","mes","pen","vic","fag","bud","ken","nod","jam","coy","hui","sue","nap","ton","coy","rut","fit","cut","eta","our","oho","zip"]) // 613
+groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
