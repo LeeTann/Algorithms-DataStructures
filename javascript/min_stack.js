da6a8ca4b952a1ed4f84af09ad132f6abb4a98c6
ltann <leeroyleeroy18@gmail.com>
2020-04-10 20:56:55 -0700

    min stack js

---
+++ javascript/min_stack.js
@@ -0,0 +1,55 @@
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+// push(x) -- Push element x onto stack.
+// pop() -- Removes the element on top of the stack.
+// top() -- Get the top element.
+// getMin() -- Retrieve the minimum element in the stack.
+ 
+
+// Example:
+
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin();   --> Returns -3.
+// minStack.pop();
+// minStack.top();      --> Returns 0.
+// minStack.getMin();   --> Returns -2.
+
+
+var MinStack = function() {
+  this.stack = [];
+};
+
+/** 
+* @param {number} x
+* @return {void}
+*/
+MinStack.prototype.push = function(x) {
+  this.stack.push(x);
+  
+};
+
+/**
+* @return {void}
+*/
+MinStack.prototype.pop = function() {
+  if (this.stack.length > 0)
+      this.stack.pop();
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.top = function() {
+  if (this.stack.length > 0)
+      return this.stack[this.stack.length - 1];
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.getMin = function() {
+  return Math.min.apply(Math, this.stack)
+};
