8a2dd47a91bb9545b5c249c4acb65ff97c954e06
ltann <leeroyleeroy18@gmail.com>
2020-12-09 22:10:46 -0800

    tiny val

--- javascript/practice.js
+++ javascript/practice.js
@@ -1,2 +1,19 @@
-lastName = 'tann'
-console.log(lastName)
+function tinyVal(array1, array2, k) {
+  if (array1.length === 0 || array2.length === 0) return 0
+
+  let count = 0
+
+  while (array1.length !== 0) {
+    let popped1 = array1.shift().toString()
+    let popped2 = array2.pop().toString()
+
+    let concatVal = popped1 + popped2
+    console.log(concatVal)
+
+    if (concatVal > k) count++
+  }
+  return count
+}
+
+console.log(tinyVal([1, 5, 8], [6, 3, 4], 40)) //2
+console.log(tinyVal([], [6, 3, 4], 40))
