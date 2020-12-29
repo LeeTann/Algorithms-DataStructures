cd36a79a87374a1957894f6965b52deb916dae49
ltann <leeroyleeroy18@gmail.com>
2020-12-29 12:10:05 -0800

    half are alike

---
+++ javascript/halves_are_alike.js
@@ -0,0 +1,23 @@
+var halvesAreAlike = function (s) {
+  let countL = 0
+  let countR = 0
+
+  let vowel = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+  let half = s.length / 2
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i]
+    if (vowel.has(char)) {
+      if (i < half) {
+        countL++
+      } else {
+        countR++
+      }
+    }
+  }
+  console.log(countL)
+  console.log(countR)
+  return countL === countR
+}
+
+console.log(halvesAreAlike('book'))
