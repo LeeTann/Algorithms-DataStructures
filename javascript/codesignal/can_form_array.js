76f9eae2026e529b86c9c97ccd81a29bf6267b0f
ltann <leeroyleeroy18@gmail.com>
2021-02-22 15:36:20 -0800

    can form array

---
+++ javascript/codesignal/can_form_array.js
@@ -0,0 +1,27 @@
+function canFormArray(arr, pieces) {
+  const map = new Map()
+
+  arr.forEach((val, i) => map.set(val, i))
+  console.log(map)
+
+  for (let piece of pieces) {
+    console.log(piece)
+
+    if (!map.has(piece[0])) return false
+
+    let index = map.get(piece[0])
+    console.log(index)
+
+    for (let i = 0; i < piece.length; i++) {
+      let curIndex = i + index
+      console.log(curIndex)
+      if (piece[i] !== arr[curIndex]) return false
+    }
+  }
+
+  return true
+}
+
+console.log(canFormArray([1, 2, 5, 3], [[3], [1, 2], [5]])) // true
+// console.log(canFormArray([1, 2, 5, 3], [[3], [2, 1], [5]])) // false
+// console.log(canFormArray([1, 2, 5, 3], [1, 3], [2, 5]))
